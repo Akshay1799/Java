f6a3719f7921204f5afa1ad7ab4080bceb18244f
Akshay Ladne <Kumawata283@gmail.com>
2025-02-08 17:54:29 +0530

    Added a program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Enter a number: ");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        if (num % 2 == 0){
+           System.out.println("Number is even");
+        }else{
+            System.out.println("Number is odd");
+        }
     }
 }
