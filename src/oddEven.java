3060bc684bd9251a7ce7dc318daf481a56caba6f
Akshay Ladne <Kumawata283@gmail.com>
2025-02-18 18:30:52 +0530

    initial commit

---
+++ src/oddEven.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+public class oddEven {
+    public static void main(String[] args) {
+
+        int[] num = new int[10];
+        System.out.print("Enter 10 numbers: ");
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i<num.length; i++){
+            num[i] = input.nextInt();
+        }
+
+        System.out.print("Even number: " );
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] % 2 == 0){
+                System.out.println(num[i]);
+            }
+
+        }
+
+        System.out.println("Odd numbers: ");
+        for (int i = 0; i<num.length; i++){
+            if (num[i] % 2 != 0) {
+                System.out.println(num[i]);
+            }
+        }
+
+    }
+}
---
+++ src/simpleInterest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class simpleInterest {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your amount: ");
+        float amount = in.nextFloat();
+
+        System.out.print("Enter your interest rate: ");
+        float interest = in.nextFloat();
+
+        System.out.print("Enter your time duration: ");
+        float time = in.nextFloat();
+
+        float si = (amount*interest*time)/100;
+
+        System.out.println("Total interest is: " + si);
+        System.out.println("Total payable amount is: " + (amount+si));
+
+    }
+}
